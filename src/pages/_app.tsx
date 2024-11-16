06320d1d3fa11cdc03a26902398da7ca6a23f1ef
Ellie <ellie@bello.lol>
2024-11-16 11:57:36 +0700

    start startup script for storacha

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,9 +5,12 @@ import {
   DynamicWidget,
 } from "@dynamic-labs/sdk-react-core";
 import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 import getConfig from "next/config";
 const { publicRuntimeConfig } = getConfig();
+import { initializeServer } from "../../lib/init";
+
+let isInitialized = false;
 
 export default function App({ Component, pageProps }: AppProps) {
   const onAuthSuccess = useCallback(async (args: any) => {
@@ -16,6 +19,21 @@ export default function App({ Component, pageProps }: AppProps) {
     } catch (error) {
       console.error("Error during signIn", error);
     }
+  }, []);
+
+  useEffect(() => {
+    const init = async () => {
+      if (!isInitialized) {
+        try {
+          await initializeServer();
+          isInitialized = true;
+        } catch (error) {
+          console.error("Failed to initialize:", error);
+        }
+      }
+    };
+
+    init();
   }, []);
 
   return (
